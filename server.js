591164592f792000809f52d8dc65b808478d9284
Brunno321 <64109065+Brunno321@users.noreply.github.com>
2026-03-23 09:23:01 -0300

    Update server.js

--- server.js
+++ server.js
@@ -150,6 +150,13 @@ Para cada tema:
 - Gerar conteúdo genérico
 - Inventar temas fora dos títulos
 - Ignorar os dados fornecidos
+
+FORMATAÇÃO OBRIGATÓRIA:
+- Usar títulos (###)
+- Usar listas com marcadores (-)
+- Cada recomendação deve ser separada
+- NÃO escrever em parágrafo único
+- NÃO usar texto corrido
 DADOS:
 ${JSON.stringify(dados)}
 `;
